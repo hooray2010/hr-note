24776754c885a3385a74723f7dd94d7ed2ecaeaf
hooray <hooray2010@foxmail.com>
2017-07-28 00:01:46 +0800

    add classpath test

---
+++ src/main/java/demo/PathTest.java
@@ -0,0 +1,15 @@
+package demo;
+
+/**
+ * Created by hurui on 2017/7/25.
+ */
+public class PathTest {
+
+  public static void main(String[] args) {
+    for (String s : args) {
+      System.out.println(s);
+    }
+    System.out.println("test classpath...");
+  }
+
+}
