6212e0ce2e1331b65fbdb005c59889888d9d8184
hurui <hooray2010@foxmail.com>
2017-11-10 00:00:41 +0800

    add proxy

---
+++ src/main/java/demo/proxy/SomeInterface.java
@@ -0,0 +1,10 @@
+package demo.proxy;
+
+/**
+ * Created by hurui on 2017/11/9.
+ */
+public interface SomeInterface {
+
+  String doSomething();
+
+}
---
+++ src/main/java/demo/proxy/SomeObject.java
@@ -0,0 +1,11 @@
+package demo.proxy;
+
+/**
+ * Created by hurui on 2017/11/9.
+ */
+public class SomeObject implements SomeInterface {
+  @Override
+  public String doSomething() {
+    return "do something";
+  }
+}
